ed9a725c2f40432bcbb8f3d5aab6cbeddbe6f4d1
malaribi <malaribi@f2r6s7.paris.42.school>
2026-07-29 11:50:21 +0200

    labyrinthe adaptable en fonction de lecran

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import pygame
 from mazegenerator import MazeGenerator
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 1000
+WIDTH = 20
+HEIGHT = 20
+RECTANGLE = SCREEN_WIDTH / WIDTH
+
 
 def show_maze():
     surface_pos_x = 0
@@ -8,34 +14,32 @@ def show_maze():
     for _ in range(len(maze_grid[0])):
         horizontal_wall.fill("White")
         screen.blit(horizontal_wall, (surface_pos_x, surface_pos_y))
-        surface_pos_x += 20
+        surface_pos_x += RECTANGLE
     surface_pos_x = 0
     for _ in range(len(maze_grid)):
         vertical_wall.fill("White")
         screen.blit(vertical_wall, (surface_pos_x, surface_pos_y))
-        surface_pos_y += 20
-    surface_pos_y = 20
+        surface_pos_y += RECTANGLE
+    surface_pos_y = RECTANGLE
     for i in maze_grid:
         surface_pos_x = 0
         for element in i:
             if ((element >> 1) & 1) != 0:
                 vertical_wall.fill("White")
-                screen.blit(vertical_wall, (surface_pos_x + 20, surface_pos_y - 20))
+                screen.blit(vertical_wall, (surface_pos_x + RECTANGLE, surface_pos_y - RECTANGLE))
             if ((element >> 2) & 1) != 0:
                 horizontal_wall.fill("White")
                 screen.blit(horizontal_wall, (surface_pos_x, surface_pos_y))
-            surface_pos_x += 20
-        surface_pos_y += 20
+            surface_pos_x += RECTANGLE
+        surface_pos_y += RECTANGLE
 
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
-vertical_wall = pygame.Surface((1, 20))
-horizontal_wall = pygame.Surface((20, 1))
-vertical_wall.fill("White")
-horizontal_wall.fill("White")
-maze_gen = MazeGenerator((200, 200))
+vertical_wall = pygame.Surface((1, RECTANGLE))
+horizontal_wall = pygame.Surface((RECTANGLE, 1))
+maze_gen = MazeGenerator((WIDTH, HEIGHT))
 maze_grid = maze_gen.maze
 shortest_path = maze_gen.shortest_path
 print(maze_grid)
