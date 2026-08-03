5bc18b954d560d8315e4f3d6978b3bb6c39e7ee7
malaribi <malaribi@f6r6s10.paris.42.school>
2026-08-03 16:51:33 +0200

    ajout du menu et ecran de pause (ce n'est qu'un prototype)

--- test_pygame.py
+++ test_pygame.py
@@ -3,7 +3,7 @@ from pygame.locals import QUIT, RESIZABLE
 from mazegenerator import MazeGenerator
 from math import ceil
 from typing import Any
-from random import randint
+from random import randint, seed
 
 
 def check(dir: int, wall: int) -> bool:
@@ -29,6 +29,7 @@ def check_opposite(dir: int, next: int) -> bool:
         return True
     if dir == 13 and next == 7:
         return True
+    return False
 
 
 def choose_dir(frame: Any, dir: int) -> Any:
@@ -43,12 +44,32 @@ def choose_dir(frame: Any, dir: int) -> Any:
     return frame
 
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 1000
+SCREEN_WIDTH = 1001
+SCREEN_HEIGHT = 1050
 WIDTH = 20
 HEIGHT = 20
 CELL_SIZE = SCREEN_WIDTH // WIDTH
 PACGUM = 5
+
+
+def main_menu():
+    windows.fill("Yellow")
+    windows.blit(start_game, start_game_rectangle)
+    windows.blit(instruction, instruction_rectangle)
+    windows.blit(title_surface, title_rectangle)
+    windows.blit(highscore, highscore_rectangle)
+    windows.blit(sortie, sortie_rectangle)
+    if choice == 0:
+        pygame.draw.rect(windows, "Red", start_game_rectangle, 1)
+    if choice == -1:
+        pygame.draw.rect(windows, "Red", start_game_rectangle, -1)
+        pygame.draw.rect(windows, "Red", instruction_rectangle, 1)
+    if choice == -2:
+        pygame.draw.rect(windows, "Red", instruction_rectangle, -1)
+        pygame.draw.rect(windows, "Red", highscore_rectangle, 1)
+    if choice == -3:
+        pygame.draw.rect(windows, "Red", highscore_rectangle, -1)
+        pygame.draw.rect(windows, "Red", sortie_rectangle, 1)
 
 
 def verif_config():
@@ -62,7 +83,7 @@ def create_pacgum(maze_grid: list[list[int]]):
 
     while len(pacgum) != PACGUM:
         width = randint(0, WIDTH - 1)
-        height =  randint(0, HEIGHT - 1)
+        height = randint(0, HEIGHT - 1)
         if maze_grid[height][width] != 15:
             pacgum.add((width, height))
 
@@ -73,7 +94,7 @@ def show_pacgum(pacgum: set[tuple[int, int]]):
         center_x = x * CELL_SIZE + CELL_SIZE / 2
         center_y = y * CELL_SIZE + CELL_SIZE / 2
 
-        objet = pygame.Surface((max(2, CELL_SIZE // 10),max(2, CELL_SIZE // 10)))
+        objet = pygame.Surface((max(2, CELL_SIZE // 10), max(2, CELL_SIZE // 10)))
         objet_rect = objet.get_rect(center=(center_x, center_y))
 
         pygame.draw.rect(windows, "Orange", objet_rect)
@@ -105,16 +126,36 @@ def show_maze():
         surface_pos_y += CELL_SIZE
 
 
-maze = MazeGenerator((WIDTH, HEIGHT))
+level = 1
+maze = MazeGenerator((WIDTH, HEIGHT), seed=4)
 maze.generate()
 wall = maze.maze
 print(wall)
 pygame.init()
-windows = pygame.display.set_mode((1010, 1010), RESIZABLE)
+windows = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
 vertical_wall = pygame.Surface((1, CELL_SIZE))
 horizontal_wall = pygame.Surface((CELL_SIZE, 1))
 vertical_wall.fill("White")
 horizontal_wall.fill("White")
+font = pygame.font.Font("Pixeltype.ttf", 100)
+h1_font = pygame.font.Font("Pixeltype.ttf", 300)
+font_text = pygame.font.Font("Pixeltype.ttf", 50)
+
+title_surface = h1_font.render("Pac-Man", False, "Red")
+title_rectangle = title_surface.get_rect(center=(SCREEN_WIDTH/2, 200))
+
+start_game = font.render("Start Game", False, (64, 64, 64))
+start_game_rectangle = start_game.get_rect(center=(SCREEN_WIDTH/2, 500))
+
+instruction = font.render("Instructions", False, (64, 64, 64))
+instruction_rectangle = instruction.get_rect(center=(SCREEN_WIDTH/2, 600))
+
+highscore = font.render("Highscore", False, (64, 64, 64))
+highscore_rectangle = highscore.get_rect(center=(SCREEN_WIDTH/2, 700))
+
+sortie = font.render("Exit", False, (64, 64, 64))
+sortie_rectangle = sortie.get_rect(center=(SCREEN_WIDTH/2, 800))
+
 # show_maze()
 # background = pygame.image.load("laby.png").convert()
 
@@ -137,6 +178,7 @@ vitesse = 5
 
 
 run = True
+score = 0
 sprite = 1
 count = 0
 clock = pygame.time.Clock()
@@ -145,77 +187,141 @@ reste_x = 0
 reste_y = 0
 verif_config()
 pacgum = create_pacgum(wall)
+active_game = False
+exit_game = False
+choice = 0
+pause = False
 while run:
     for event in pygame.event.get():
-            if event.type == QUIT:
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if choice == 0 and pause is False:
+                if event.key == pygame.K_UP:
+                    choice = 0
+                if event.key == pygame.K_DOWN:
+                    choice = -1
+            elif choice == -1 and pause is False:
+                if event.key == pygame.K_UP:
+                    choice = 0
+                if event.key == pygame.K_DOWN:
+                    choice = -2
+            elif choice == -2 and pause is False:
+                if event.key == pygame.K_UP:
+                    choice = -1
+                if event.key == pygame.K_DOWN:
+                    choice = -3
+            elif choice == -3 and pause is False:
+                if event.key == pygame.K_UP:
+                    choice = -2
+                if event.key == pygame.K_DOWN:
+                    choice = -3
+            if choice == 0 and event.key == pygame.K_RETURN and pause is False:
+                active_game = True
+            if choice == -3 and event.key == pygame.K_RETURN and pause is False:
                 pygame.quit()
                 exit()
-    count += 1
-    # player = frames[count % 4]
-    player = choose_dir(frames[count % 4], dir)
-    player_rect = pygame.Rect(player_x, player_y, player.get_width(), player.get_height())
-    clock.tick(30)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        # next_dir = "left"
-        next_dir = 7
-        # player = pygame.transform.rotate(frames[1], 180)
-        # frames = ["a/pac0.png", "a/pac1.png", "a/pac2.png", "a/pac3.png"]
-    if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        # next_dir = "right"
-        next_dir = 13
-        # frames = ["d/pac0.png", "d/pac1.png", "d/pac2.png", "d/pac3.png"]
-    if keys[pygame.K_w] or keys[pygame.K_UP]:
-        # next_dir = "up"
-        next_dir = 14
-        # frames = ["w/pac0.png", "w/pac1.png", "w/pac2.png", "w/pac3.png"]
-    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        # next_dir = "down"
-        next_dir = 11
-        # frames = ["s/pac0.png", "s/pac1.png", "s/pac2.png", "s/pac3.png"]
-    if keys[pygame.K_t]:
-        run = False
-    if check_opposite(dir, next_dir):
-        dir = next_dir
-    if check(next_dir, wall[Y][X]) and reste_x == 10 and reste_y == 10:
-        dir = next_dir
-    if dir == 7 and player_x > 10:
-        if (wall[Y][X] >> 3) & 1 == 1 and reste_x == 10 and reste_y == 10:
-            pass
-        else:
-            player_x -= vitesse
-    if dir == 13 and player_x < SCREEN_WIDTH - CELL_SIZE + 10:
-        if (wall[Y][X] >> 1) & 1 == 1 and reste_x == 10 and reste_y == 10:
-            pass
-        else:
-            player_x += vitesse
-    if dir == 14 and player_y > 10:
-        if (wall[Y][X] >> 0) & 1 == 1 and reste_x == 10 and reste_y == 10:
-            pass
-        else:
-            player_y -= vitesse
-    if dir == 11 and player_y < SCREEN_HEIGHT - CELL_SIZE + 10:
-        if (wall[Y][X] >> 2) & 1 == 1 and reste_x == 10 and reste_y == 10:
-            pass
-        else:
-            player_y += vitesse
-    X = player_rect.centerx // CELL_SIZE
-    Y = player_rect.centery // CELL_SIZE
-    reste_x = player_x % CELL_SIZE
-    reste_y = player_y % CELL_SIZE
-    # windows.blit(background, (0, 0))
-    # pygame.display.flip()
-    # print(f"X={X} Y={Y}")
-    print(f"X={player_x} Y={player_y} X={X} Y={Y} resteX={reste_x} restY= {reste_y} dir= {dir} cell= {wall[Y][X]}")
-    windows.fill((0, 0, 0))
-    show_maze()
-    show_pacgum(pacgum)
-    if (X, Y) in pacgum:
-        pacgum.remove((X, Y))
+            if event.key == pygame.K_ESCAPE:
+                pause = True
+                if pause:
+                    choice = 0
+                    continue
+            if choice == 0 and pause is True:
+                if event.key == pygame.K_UP:
+                    choice = 0
+                if event.key == pygame.K_DOWN:
+                    choice = -1
+            elif choice == -1 and pause is True:
+                if event.key == pygame.K_UP:
+                    choice = 0
+                if event.key == pygame.K_DOWN:
+                    choice = -1
+            if choice == 0 and event.key == pygame.K_RETURN and pause is True:
+                pause = False
+    if active_game:
+        if pause:
+            windows.fill("Blue")
+            if choice == 0:
+                pygame.draw.rect(windows, "Red", start_game_rectangle, 1)
+            if choice == -1:
+                pygame.draw.rect(windows, "Red", start_game_rectangle, -1)
+                pygame.draw.rect(windows, "Red", instruction_rectangle, 1)
+            pygame.display.update()
+            continue
+        count += 1
+        # player = frames[count % 4]
+        player = choose_dir(frames[count % 4], dir)
+        player_rect = pygame.Rect(player_x, player_y, player.get_width(), player.get_height())
+        clock.tick(30)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            # next_dir = "left"
+            next_dir = 7
+            # player = pygame.transform.rotate(frames[1], 180)
+            # frames = ["a/pac0.png", "a/pac1.png", "a/pac2.png", "a/pac3.png"]
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            # next_dir = "right"
+            next_dir = 13
+            # frames = ["d/pac0.png", "d/pac1.png", "d/pac2.png", "d/pac3.png"]
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            # next_dir = "up"
+            next_dir = 14
+            # frames = ["w/pac0.png", "w/pac1.png", "w/pac2.png", "w/pac3.png"]
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            # next_dir = "down"
+            next_dir = 11
+            # frames = ["s/pac0.png", "s/pac1.png", "s/pac2.png", "s/pac3.png"]
+        if keys[pygame.K_t]:
+            run = False
+        if check_opposite(dir, next_dir):
+            dir = next_dir
+        if check(next_dir, wall[Y][X]) and reste_x == 10 and reste_y == 10:
+            dir = next_dir
+        if dir == 7 and player_x > 10:
+            if (wall[Y][X] >> 3) & 1 == 1 and reste_x == 10 and reste_y == 10:
+                pass
+            else:
+                player_x -= vitesse
+        if dir == 13 and player_x < SCREEN_WIDTH - CELL_SIZE + 10:
+            if (wall[Y][X] >> 1) & 1 == 1 and reste_x == 10 and reste_y == 10:
+                pass
+            else:
+                player_x += vitesse
+        if dir == 14 and player_y > 10:
+            if (wall[Y][X] >> 0) & 1 == 1 and reste_x == 10 and reste_y == 10:
+                pass
+            else:
+                player_y -= vitesse
+        if dir == 11 and player_y < SCREEN_HEIGHT - CELL_SIZE + 10:
+            if (wall[Y][X] >> 2) & 1 == 1 and reste_x == 10 and reste_y == 10:
+                pass
+            else:
+                player_y += vitesse
+        X = player_rect.centerx // CELL_SIZE
+        Y = player_rect.centery // CELL_SIZE
+        reste_x = player_x % CELL_SIZE
+        reste_y = player_y % CELL_SIZE
+        # windows.blit(background, (0, 0))
+        # pygame.display.flip()
+        # print(f"X={X} Y={Y}")
+        print(f"X={player_x} Y={player_y} X={X} Y={Y} resteX={reste_x} restY= {reste_y} dir= {dir} cell= {wall[Y][X]}")
+        windows.fill((0, 0, 0))
+        show_maze()
         show_pacgum(pacgum)
-    if pacgum == set():
-        pygame.quit()
-        exit()
-    windows.blit(player, (player_x, player_y))
-    pygame.display.flip()
+        if (X, Y) in pacgum:
+            score += 10
+            pacgum.remove((X, Y))
+            show_pacgum(pacgum)
+        if pacgum == set():
+            choice = 0
+            active_game = False
+        windows.blit(player, (player_x, player_y))
+        score_surface = font_text.render(f"{score}", False, (64, 64, 64))
+        score_rect = score_surface.get_rect(bottomright=(990, 1050))
+        windows.blit(score_surface, score_rect)
+        pygame.display.flip()
+    else:
+        main_menu()
+    pygame.display.update()
 pygame.quit()
